096e4edd29aeaa3010a9607205da7eee47e61b26
Kevin Doyle <kdoyle2@gmail.com>
2011-06-20 14:13:53 -0400

    add MIT license info

--- jquery.cycleItem.js
+++ jquery.cycleItem.js
@@ -1,3 +1,11 @@
+/**
+* jQuery Cycle Item
+* Copyright (c) 2011 Kevin Doyle
+* Dual licensed under the MIT and GPL licenses:
+* http://www.opensource.org/licenses/mit-license.php
+* http://www.gnu.org/licenses/gpl.html
+**/
+
 (function($){
 	var config = {
 		delay: 10000,
